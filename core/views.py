4b5c302c2e4dcc5771ec14d375f60b5293e8ab61
EJECT <projetos@ejectufrn.com.br>
2018-05-17 14:39:58 -0300

    First commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+# from io import BytesIO
+# from django.http import HttpResponse
+# from django.template.loader import get_template
+# import xhtml2pdf.pisa as pisa
+from easy_pdf.views import PDFTemplateView
+
+# def pdf_generation(request):
+# 	template = get_template('templates/index.html')
+# 	html = template.render()
+# 	response = BytesIO()
+# 	pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response)
+# 	if not pdf.err:
+# 		return HttpResponse(response.getvalue(), content_type='application/pdf')
+# 	else:
+# 		return HttpResponse("Error Rendering PDF", status=400)
+
+class HelloPDFView(PDFTemplateView):
+    template_name = 'templates/index.html'
